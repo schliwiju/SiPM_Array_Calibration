a9a63c1e22fa401d7ba209683e8dd082b67f1011
schliwiju <julian@schliwi.de>
2018-12-19 12:49:16 +0100

    added compiler options for stability

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm read
-g++ geometry.C read.C analysis.C main.C -rpath ${ROOTSYS}/lib `root-config --libs --cflags` -o read # -rpath option might be necessary with some ROOT installations 
+# -rpath option might be necessary with some ROOT installations 
+# -lSpectrum option might be necessary with some ROOT installations 
+g++ geometry.C read.C analysis.C main.C -rpath ${ROOTSYS}/lib `root-config --libs --cflags` -lSpectrum -o read 
 #compile for memory test
 #g++ read.C `root-config --libs --cflags` -o read  -ltcmalloc
